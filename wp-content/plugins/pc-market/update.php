6f85e5488f595f35017a6deab71da12e9bffeba0
kuba <info@weblead.pl>
2022-09-14 11:07:05 +0200

    commit

--- wp-content/plugins/pc-market/update.php
+++ wp-content/plugins/pc-market/update.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('max_execution_time', '600');
+set_time_limit(600);
+
+global $counter;
+$counter=0;
+
 	 function do_ajax_product_import($file) {
 		global $wpdb;
 
@@ -37,12 +43,12 @@
 			'delimiter'       => ',', 
 			'start_pos'       => 0, // PHPCS: input var ok.
 			'mapping'         => array(
-                'from' => array("towar_id","kod","cku","attribute_1_name","attribute_1_value","attribute_1_visible","attribute_2_name","attribute_2_value","attribute_2_visible","nazwa","skrot","attribute_3_name","attribute_3_value","attribute_3_visible","attribute_4_name","attribute_4_value","attribute_4_visible","attribute_5_name","attribute_5_value","attribute_5_visible","attribute_6_name","attribute_6_value","attribute_6_visible","attribute_7_name","attribute_7_value","attribute_7_visible","attribute_8_name","attribute_8_value","attribute_8_visible","waga","wysokosc","szerokosc","glebokosc","attribute_9_name","attribute_9_value","attribute_9_visible","attribute_10_name","attribute_10_value","attribute_10_visible","attribute_11_name","attribute_11_value","attribute_11_visible","attribute_12_name","attribute_12_value","attribute_12_visible","il_kg_litrow","kod_CN","podlega_OO","podlega_MPP","status_do_zamowien","nowosc_od","nowosc_przez","koszty_transportu","koszty_przechowywania","koszty_inne","min_cena_sprzedazy_PCPOS","aktywny_w_SI","nazwa_w_SI","cena_zakupu","cena_detal","cena_hurtowa","cena_nocna","cena_dodatkowa","cena_detal_przed_prom","cena_hurtowa_przed_prom","cena_nocna_przed_prom","cena_dodatkowa_przed_prom","marza_suger","narzut_nocny","rabat_hurtowy","rabat_dodatkowy","status_ceny","opakowanie_id","ilosc_w_opakowaniu","czy_tandem","czy_karton","czy_artykul","artykul_id","kategoria_id","producent_id","dostawca_id","dost_id","dost_id2","opis1","opis2","opis3","opis4","notatki","uwagi_do_dostaw","nr_drukarki","folder_zdjec","plik_zdjecia","magazyn_id2","stan_magazynu2","stan_magazynu_min2","stan_magazynu_max2","blokada_stanu2","rezerwacja_ilosci2","magazyn_id3","stan_magazynu3","stan_magazynu_min3","stan_magazynu_max3","blokada_stanu3","rezerwacja_ilosci3","magazyn_id4","stan_magazynu4","stan_magazynu_min4","stan_magazynu_max4","blokada_stanu4","rezerwacja_ilosci4","magazyn_id","stan_magazynu","stan_magazynu_min","stan_magazynu_max","blokada_stanu","rezerwacja_ilosci","kod2","ilosc_w_kodzie2","opis5","poziom_cen2","kod3","ilosc_w_kodzie3","opis6","poziom_cen3","kod4","ilosc_w_kodzie","opis","poziom_cen","parametry","data_aktualizacji","visibility","type"),
+                'from' => array("my_sku","towar_id","kod","cku","attribute_1_name","attribute_1_value","attribute_1_visible","attribute_2_name","attribute_2_value","attribute_2_visible","nazwa","skrot","attribute_3_name","attribute_3_value","attribute_3_visible","attribute_4_name","attribute_4_value","attribute_4_visible","attribute_5_name","attribute_5_value","attribute_5_visible","attribute_6_name","attribute_6_value","attribute_6_visible","attribute_7_name","attribute_7_value","attribute_7_visible","attribute_8_name","attribute_8_value","attribute_8_visible","waga","wysokosc","szerokosc","glebokosc","attribute_9_name","attribute_9_value","attribute_9_visible","attribute_10_name","attribute_10_value","attribute_10_visible","attribute_11_name","attribute_11_value","attribute_11_visible","attribute_12_name","attribute_12_value","attribute_12_visible","il_kg_litrow","kod_CN","podlega_OO","podlega_MPP","status_do_zamowien","nowosc_od","nowosc_przez","koszty_transportu","koszty_przechowywania","koszty_inne","min_cena_sprzedazy_PCPOS","aktywny_w_SI","nazwa_w_SI","cena_zakupu","cena_detal","cena_hurtowa","cena_nocna","cena_dodatkowa","cena_detal_przed_prom","cena_hurtowa_przed_prom","cena_nocna_przed_prom","cena_dodatkowa_przed_prom","marza_suger","narzut_nocny","rabat_hurtowy","rabat_dodatkowy","status_ceny","opakowanie_id","ilosc_w_opakowaniu","czy_tandem","czy_karton","czy_artykul","artykul_id","kategoria_id","producent_id","dostawca_id","dost_id","dost_id2","opis1","opis2","opis3","opis4","notatki","uwagi_do_dostaw","nr_drukarki","folder_zdjec","plik_zdjecia","magazyn_id2","stan_magazynu2","stan_magazynu_min2","stan_magazynu_max2","blokada_stanu2","rezerwacja_ilosci2","magazyn_id3","stan_magazynu3","stan_magazynu_min3","stan_magazynu_max3","blokada_stanu3","rezerwacja_ilosci3","magazyn_id4","stan_magazynu4","stan_magazynu_min4","stan_magazynu_max4","blokada_stanu4","rezerwacja_ilosci4","magazyn_id","stan_magazynu","stan_magazynu_min","stan_magazynu_max","blokada_stanu","rezerwacja_ilosci","kod2","ilosc_w_kodzie2","opis5","poziom_cen2","kod3","ilosc_w_kodzie3","opis6","poziom_cen3","kod4","ilosc_w_kodzie","opis","poziom_cen","parametry","data_aktualizacji","visibility","type"),
 
-                'to' => array("","sku","","attributes:name1","attributes:value1","attributes:visible1","attributes:name2","attributes:value2","attributes:visible2","name","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","type")
+                'to' => array("sku","","","","attributes:name1","attributes:value1","attributes:visible1","attributes:name2","attributes:value2","attributes:visible2","name","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","type")
 
 
-            ), // PHPCS: input var ok.
+            ), // PHPCS: input var ok.ff
 			'update_existing' => true, // PHPCS: input var ok.
 			'lines'           => 30,
 			'parse'           => true,
@@ -159,9 +165,11 @@ while (false !== ($entry = readdir($handle))) {
 
 
 		$adr= '/home4/smakolyk/' . $en;
-
+		$counter++;
     do_ajax_product_import($adr);
 
 	}
+
+	
 
 ?>
