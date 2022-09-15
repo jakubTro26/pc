98c039e1c6ba713e326752736231492987c3a33b
kuba <info@weblead.pl>
2022-09-15 15:18:17 +0200

    commit

--- wp-content/plugins/pc-market/pc-market.php
+++ wp-content/plugins/pc-market/pc-market.php
@@ -120,7 +120,7 @@ function mt_toplevel_page() {
                 $.ajax({
                 type: "POST",
                 contentType: "application/json; charset=utf-8",
-                url: "../wp-content",
+                url: " https://smakolyk.nstrefa.pl/wp-content/plugins",
                 data: { data1: window.csv },
                  success: function (result) {
            // do something here
