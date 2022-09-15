a1231c51adbba424f299abcd0a13fa75bada46bf
kuba <info@weblead.pl>
2022-09-15 12:40:26 +0200

    commit

--- wp-content/plugins/pc-market/import.js
+++ wp-content/plugins/pc-market/import.js
@@ -1,6 +1,6 @@
 function importing(){
 
-    import * as fs from 'fs';
+    import * as fs from '/home4/smakolyk/public_html/pcwordpress/node_modules';
 
     var loc = window.location.pathname;
     var dir = loc.substring(0, loc.lastIndexOf('/'));
