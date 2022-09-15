933202f2ef0b16bf79d03a5db14d9b02a14c313e
kuba <info@weblead.pl>
2022-09-15 07:11:08 +0200

    commit

--- wp-content/plugins/pc-market/import.js
+++ wp-content/plugins/pc-market/import.js
@@ -1,4 +1,14 @@
 function importing(){
 
-console.log('kuba');
+    const fs = require('fs');
+
+    var loc = window.location.pathname;
+    var dir = loc.substring(0, loc.lastIndexOf('/'));
+
+    fs.readdir(dir, (err, files) => {
+        files.forEach(file => {
+          console.log(file);
+        });
+      });
+
 }
