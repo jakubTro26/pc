f8afa48d1b99854d973349870fb6f5d31c5c9ce0
kuba <info@weblead.pl>
2022-09-15 14:33:40 +0200

    commit

--- wp-content/plugins/pc-market/pc-market.php
+++ wp-content/plugins/pc-market/pc-market.php
@@ -113,16 +113,16 @@ function mt_toplevel_page() {
                 var count = window.csv.length;
                 var input_c = count/5;
                 
-                // $.ajax({
-                //     type: "POST",
-                //     contentType: "application/json; charset=utf-8",
-                //     url: "",
-                //     data: { data1: window.csv },
-                // success: function (result) {
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json; charset=utf-8",
+                    url: "",
+                    data: { data1: window.csv },
+                success: function (result) {
          
       
-                //   }
-                // });
+                  }
+                });
             };
 
 
