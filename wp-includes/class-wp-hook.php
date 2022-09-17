fa1d9cdf5f56afda54849c73174e71b7727820f4
kuba <info@weblead.pl>
2022-09-17 16:08:29 +0200

    commit

--- wp-includes/class-wp-hook.php
+++ wp-includes/class-wp-hook.php
@@ -285,7 +285,7 @@ final class WP_Hook implements Iterator, ArrayAccess {
 
 
 		if($value=='woocommerce_product_csv_importer_class'){
-			echo 'mata';
+			var_dump($this);
 		}
 
 		if ( ! $this->callbacks ) {
