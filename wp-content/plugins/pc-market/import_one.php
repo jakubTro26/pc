c6242fd1c6a90e0a9006be81d44e61b0db3cdbc0
kuba <info@weblead.pl>
2022-09-17 13:44:39 +0200

    commit

--- wp-content/plugins/pc-market/import_one.php
+++ wp-content/plugins/pc-market/import_one.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 
 
-		include_once  '/home4/smakolyk/pcwordpress/wp-admin/includes/class-wp-importer.php';
+		include_once  '/home4/smakolyk/public_html/pcwordpress/wp-admin/includes/class-wp-importer.php';
 		include_once  WC_ABSPATH . 'includes/admin/importers/class-wc-product-csv-importer-controller.php';
 		include_once  WC_ABSPATH . 'includes/import/class-wc-product-csv-importer.php';
         
